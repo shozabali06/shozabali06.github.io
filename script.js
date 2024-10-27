877d543c291dba5d2b3f4578fed6736b93f82b3c
Shozab <shozabali06@gmail.com>
2024-10-27 09:37:57 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value == "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveTasks();
+}
+
+document.getElementById("input-box").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+      event.preventDefault(); // Prevent form submission if inside a form
+      addTask(); // Call your addTask function
+  }
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveTasks();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveTasks();
+    }
+  },
+  false
+);
+
+function saveTasks() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTasks() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
